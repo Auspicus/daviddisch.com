c262cb5caa08ddcd506d2a2c25df965ca931a215
David Disch <elijahdisch@gmail.com>
2021-10-17 18:29:38 +1000

    Copy _redirects

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ const CleanCSS = require('clean-css')
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('img')
   eleventyConfig.addPassthroughCopy('css')
+  eleventyConfig.addPassthroughCopy('_redirects')
   eleventyConfig.addPlugin(eleventyNavigationPlugin)
   eleventyConfig.addFilter('cssmin', (code) => {
     return new CleanCSS({
