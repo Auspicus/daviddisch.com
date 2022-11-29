e501b5120d30ae57747e854b5778b49954a02be7
David Disch <elijahdisch@gmail.com>
2022-11-30 10:40:49 +1100

    Add gtag

--- components/SEO/index.tsx
+++ components/SEO/index.tsx
@@ -2,17 +2,25 @@ import React from 'react'
 import Head from 'next/head'
 import { Meta } from '../../types'
 
+declare global {
+  interface Window {
+    dataLayer: any[]
+  }
+}
+
 const SEO: React.FC<{ title: string, meta?: Meta[] }> = ({ title, meta = [] }) => {
   if (!meta.some(m => m.name === 'description')) {
     meta.push({ name: 'description', content: 'Software engineer, surfer, climber, photographer.' })
   }
 
   return (
-    <Head>
-      <title>{`${title} - David Disch | Software Engineer`}</title>
-      {meta.map(m => <meta key={m.name} name={m.name} content={m.content} />)}
-      <link rel="icon" href="/img/favicon.ico" type="image/x-icon" />
-    </Head>
+    <>
+      <Head>
+        <title>{`${title} - David Disch | Software Engineer`}</title>
+        {meta.map(m => <meta key={m.name} name={m.name} content={m.content} />)}
+        <link rel="icon" href="/img/favicon.ico" type="image/x-icon" />
+      </Head>
+    </>
   )
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
+import React, { useEffect } from 'react'
 import '../styles/globals.css'
 
+declare global {
+  interface Window {
+    gtag: (...args: any[]) => void
+  }
+}
+
 function Root({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag('event', 'page_view', {
+        page_title: document.title,
+        page_path: url,
+      })
+    };
+    
+    router.events.on('routeChangeComplete', handleRouteChange);
+    
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+  
+  return <>
+    <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=UA-104421109-1" />
+    <Script id="google-analytics" strategy="afterInteractive" dangerouslySetInnerHTML={{
+      __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-104421109-1');
+      `
+    }} />
+    <Component {...pageProps} />
+  </>
 }
 
 export default Root
