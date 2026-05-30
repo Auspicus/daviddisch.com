6e7e27a3edff92d14fb9b7ec5dfec2f10a690b20
Auspicus <elijahdisch@gmail.com>
2026-05-30 21:10:24 +1000

    use vendored openssl, add x86 build target with cargo zigbuild

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+TARGET=${1:-current}
+
 # Install Rust if not present
 if ! command -v cargo &>/dev/null; then
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain stable
@@ -14,7 +16,13 @@ fi
 
 # Fetch Notion content
 pushd "$(dirname "$0")/fetcher"
-cargo run --release
+if [[ $TARGET = "x86" ]]; then
+    echo "building for x86"
+    cargo zigbuild --target x86_64-unknown-linux-gnu --release
+else
+    echo "building for current target"
+    cargo run --release
+fi
 popd
 
 # Build site
